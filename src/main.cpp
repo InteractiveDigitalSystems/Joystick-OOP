1b038ee313d59b4f622f88f54a1c38daf06135ca
faaip <frederik@tollund.cc>
2020-10-01 12:15:02 +0200

    printing in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
 #include <Arduino.h>
 #include <Joystick.h>
 
-Joystick joystick = Joystick(39,36,15); // Instantiate object
-
+Joystick joystick = Joystick(36, 39, 15); // Instantiate object
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  // Serial.println(joystick.isInDeadzone());
-  Serial.println(joystick.isButtonPressed());
-}
+// void printJoystickState(Joystick *joystick) {
+//   // put your main code here, to run repeatedly:
+//   Serial.print("Button state: ");
+//   Serial.print(joystick->isButtonPressed());
+//   if (joystick->isInDeadzone() == false) {
+//     Serial.print(" , X ");
+//     Serial.print(joystick->getJoystickValues().first);
+//     Serial.print(" , y ");
+//     Serial.print(joystick->getJoystickValues().second);
+//   }
+//   Serial.println();
+// }
+
+void loop() { 
+  // printJoystickState(&joystick);
+
+    // put your main code here, to run repeatedly:
+    Serial.print("Button state: ");
+    Serial.print(joystick.isButtonPressed());
+    if (joystick.isInDeadzone() == false) {
+      Serial.print(" , X ");
+      Serial.print(joystick.getJoystickValues().first);
+      Serial.print(" , y ");
+      Serial.print(joystick.getJoystickValues().second);
+    }
+    Serial.println();
+  }
